1dc17847a3081df5d5dd7aede597500b09b5c423
小莫 <xiaomo1433675239@outlook.com>
2023-08-21 15:30:11 +0800

    修复 部分样式错误的问题

--- model/marked.ts
+++ model/marked.ts
@@ -5,16 +5,16 @@ marked.setOptions({
     breaks: true,
 })
 
-const link: marked.TokenizerExtension | marked.RendererExtension | (marked.TokenizerExtension & marked.RendererExtension) = {
-    name: 'link',
-    // start(this, src: string) { return 1; }, // 提示 Marked.js 停止并检查是否匹配
-    renderer(this: marked.RendererThis, token: marked.Tokens.Generic): string {
-        return `<a href="${token.href}" target="_blank">${token.text}</a> `;
-    }
-}
+// const link: marked.TokenizerExtension | marked.RendererExtension | (marked.TokenizerExtension & marked.RendererExtension) = {
+//     name: 'link',
+//     start(this, src: string) { return 1; }, // 提示 Marked.js 停止并检查是否匹配
+//     renderer(this: marked.RendererThis, token: marked.Tokens.Generic): string {
+//         return `<a href="${token.href}" target="_blank">${token.text}</a> `;
+//     }
+// }
 
-marked.use({
-    extensions: [link]
-})
+// marked.use({
+//     extensions: [link]
+// })
 
 export default marked;
